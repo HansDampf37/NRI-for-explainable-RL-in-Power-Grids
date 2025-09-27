4fcf2606b20c6a7af62482fcf6f971c24e0f6ac3
Adrian Degenkolb <adrian@degenkolb.de>
2025-09-27 11:23:00 +0200

    code improvements

--- baselines/train_stable_dqn_baseline.py
+++ baselines/train_stable_dqn_baseline.py
@@ -1,16 +1,13 @@
 import logging
 from pathlib import Path
-from typing import List, Dict, Optional
+from typing import Optional
 
 import grid2op
-import numpy as np
-import torch
-from grid2op.Action import TopologySetAction
-from grid2op.Observation import BaseObservation
 from grid2op.gym_compat import DiscreteActSpace
 from stable_baselines3 import DQN
 
-from baselines.baseline_agent import BaselineAgent, TopologyPolicy, evaluate
+from baselines.baseline_agent import BaselineAgent, evaluate
+from baselines.topo_policy_stable_dqn import TopoPolicyStableDQN
 from common import Grid2OpEnvWrapper, GraphObservationSpace
 from common.GNN import SB3GNNWrapper
 from common.graph_structured_observation_space import NODE_FEATURES, EDGE_FEATURES, EDGE_INDEX
@@ -25,47 +22,6 @@ _safe_max_rho = 0.9
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-
-class TopoPolicyStableDQN(TopologyPolicy):
-    """
-    Implements the interface of a topology policy. Uses a DQN trained by the stable baselines 3 library.
-    """
-
-    def __init__(self, dqn: DQN):
-        """
-        Initializes the topology policy that can be used to search for promising moves
-
-        :param dqn: The sb3 DQN
-        """
-        TopologyPolicy.__init__(self)
-        self.dqn: DQN = dqn
-        if not hasattr(dqn.observation_space, "to_gym") or not hasattr(dqn.action_space, "from_gym"):
-            raise ValueError("In order to use your DQN in a grid2op topo policy the DQN observation space needs a"
-                             "method 'to_gym' and the action space needs a method 'from_gym'")  # TODO bad design
-
-    def get_k_best_actions(self, observation: BaseObservation, k: int = 3) -> List[TopologySetAction]:
-        # Convert observation to a tensor
-        gym_obs = self.dqn.observation_space.to_gym(observation)
-        if isinstance(gym_obs, np.ndarray):
-            obs_batch = torch.from_numpy(gym_obs).unsqueeze(0).to(dtype=torch.float32)
-        elif isinstance(gym_obs, Dict):
-            obs_batch = {}
-            for key, value in gym_obs.items():
-                obs_batch[key] = torch.from_numpy(value).unsqueeze(0).to(dtype=torch.float32)
-        else:
-            raise NotImplementedError(f"Unknown gym obs type {gym_obs.__class__.__name__}")
-
-        # Get Q-values for each action
-        with torch.no_grad():
-            features = self.dqn.policy.q_net.features_extractor(obs_batch)
-            q_values = self.dqn.policy.q_net.q_net(features)
-            q_values = q_values.squeeze().cpu().numpy()
-        # Get the indices of the top k actions based on their Q-values
-        top_k_indices = np.argsort(q_values)[-k:][::-1]  # Sort in descending order
-        top_k_actions = [self.dqn.action_space.from_gym(idx) for idx in top_k_indices]
-
-        return top_k_actions
 
 
 def build_agent(load_weights_from: Path = _model_path, env_name: str = _default_env_name) -> BaselineAgent:
@@ -146,7 +102,7 @@ if __name__ == '__main__':
     evaluate(
         agent=build_agent(_model_path, _default_env_name),
         env=grid2op.make(_default_env_name + "_test"),
-        num_episodes=3,
-        max_episode_length=20,
+        num_episodes=50,
+        max_episode_length=2000,
         path_results=f"data/evaluations/stable-baselines/{_model_name}"
     )
