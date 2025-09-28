5a5292046f3647f0d82a07e07ea9450835303c68
Adrian Degenkolb <adrian@degenkolb.de>
2025-09-28 14:35:40 +0200

    FIx tests

--- tests/test_train_dqn.py
+++ tests/test_train_dqn.py
@@ -1,19 +1,25 @@
 import unittest
 
 import grid2op
+import hydra
 from grid2op.Action import BaseAction
+from omegaconf import DictConfig
 
 from baselines.train_stable_dqn_baseline import model_setup, build_agent
 
 
 class TestTrainDQN(unittest.TestCase):
     def test_model_setup(self):
-        dqn = model_setup()
-        dqn.learn(10)
+        with hydra.initialize(config_path="../hydra_configs", version_base="1.3"):
+            env_cfg = hydra.compose(config_name="config")
+            dqn = model_setup(env_cfg)
+            dqn.learn(10)
 
     def test_build_agent(self):
-        agent = build_agent(env_name="l2rpn_case14_sandbox", load_weights_from=None)
-        agent.topology_policy.get_k_best_actions = lambda _1, _2: []
-        env = grid2op.make("l2rpn_case14_sandbox")
-        agent_action = agent.act(env.current_obs, 0)
-        self.assertIsInstance(agent_action, BaseAction)
+        with hydra.initialize(config_path="../hydra_configs", version_base="1.3"):
+            cfg = hydra.compose(config_name="config")
+            agent = build_agent(cfg, load_weights_from=None)
+            agent.topology_policy.get_k_best_actions = lambda _1, _2: []
+            env = grid2op.make("l2rpn_case14_sandbox")
+            agent_action = agent.act(env.current_obs, 0)
+            self.assertIsInstance(agent_action, BaseAction)
