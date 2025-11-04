a6a37423aa247afd8e27856ca045cd4d107498eb
Adrian Degenkolb <adrian@degenkolb.de>
2025-11-04 10:59:45 +0100

    update baseline config

--- slurm_scripts/train_sb3_dqn_gnn.sh
+++ slurm_scripts/train_sb3_dqn_gnn.sh
@@ -1,9 +1,9 @@
 sbatch <<'EOF'
 #!/bin/bash
 
-#SBATCH --job-name=train_baseline_gnn                                  # Name of the job
-#SBATCH --output=out/train_baseline_gnn_v2.%j.log                      # Output file
-#SBATCH --error=out/error_train_baseline_gnn_v2.%j.log                 # Error file
+#SBATCH --job-name=train_sb3_gnn_dqn                                  # Name of the job
+#SBATCH --output=out/baseline/train_sb3_gnn_dqn.%j.log                      # Output file
+#SBATCH --error=out/baseline/error_train_sb3_gnn_dqn.%j.log                 # Error file
 #SBATCH --ntasks=1                                                     # Number of tasks
 #SBATCH --gres=gpu:1                                                   # Request 1 GPU
 #SBATCH --time=24:00:00                                                # Max wall time (HH:MM:SS)
@@ -15,5 +15,5 @@ cd ..
 module load devel/miniforge
 conda activate RL
 
-python test_cuda.py && PYTHONPATH=$(pwd) python baselines/train_stable_baseline.py baseline=gnn_dqn env.safe_max_rho=0.98 baseline.model.name=gnn-dqn-rho98-mazereward_uc3
+python test_cuda.py && PYTHONPATH=$(pwd) python baselines/train_stable_baseline.py baseline=gnn_dqn env.safe_max_rho=0.95 baseline.model.name=sb3-gnn-dqn-case14
 EOF
