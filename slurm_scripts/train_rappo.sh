aff0ac8d3976e48270eb530248512c15b8d36113
Adrian Degenkolb <adrian@degenkolb.de>
2025-11-07 14:35:05 +0100

    do prior sweep in slurm script

--- slurm_scripts/train_rappo.sh
+++ slurm_scripts/train_rappo.sh
@@ -1,19 +1,25 @@
-sbatch << 'EOF'
 #!/bin/bash
 
-#SBATCH --job-name=rappo                                                 # Name of the job
-#SBATCH --output=out/relations_aware/rappo.%j.log                        # Output file
-#SBATCH --error=out/relations_aware/error_rappo.%j.log                   # Error file
-#SBATCH --ntasks=1                                                       # Number of tasks
-#SBATCH --gres=gpu:1                                                     # Request 1 GPU
-#SBATCH --time=48:00:00                                                  # Max wall time (HH:MM:SS)
-#SBATCH --mem=64G                                                        # Memory requirement
-#SBATCH --partition=gpu_h100,gpu_a100_il,gpu_h100_il #,gpu_mi300         # Specify the GPU partition
+priors=(1.0 0.9 0.8 0.7 0.5 0.25 0.0)
+
+for p in "${priors[@]}"; do
+sbatch << EOF
+#!/bin/bash
+
+#SBATCH --job-name=rappo_${p}                                           # Name of the job
+#SBATCH --output=out/relations_aware/rappo_${p}.%j.log                  # Output file
+#SBATCH --error=out/relations_aware/error_rappo_${p}.%j.log             # Error file
+#SBATCH --ntasks=1                                                      # Number of tasks
+#SBATCH --gres=gpu:1                                                    # Request 1 GPU
+#SBATCH --time=48:00:00                                                 # Max wall time (HH:MM:SS)
+#SBATCH --mem=64G                                                       # Memory requirement
+#SBATCH --partition=gpu_h100,gpu_a100_il,gpu_h100_il #,gpu_mi300        # Specify the GPU partition
 
 cd ..
 
 module load devel/miniforge
 conda activate RL
 
-python test_cuda.py && PYTHONPATH=$(pwd) python nri/agent/ppo/train_RAPPO.py
+python test_cuda.py && PYTHONPATH=\$(pwd) python nri/agent/ppo/train_RAPPO.py rl.model.name_suffix=${p} rl.model.prior_for_graph_edges_existing=${p}
 EOF
+done
