571d0d1d944eadb01068cb6d5ceadb87c0a535e1
Adrian Degenkolb <adrian@degenkolb.de>
2025-11-10 11:46:40 +0100

    Increase memory requirements for baseline jobs to prevent failures

--- slurm_scripts/train_baseline_dqn_mlp.sh
+++ slurm_scripts/train_baseline_dqn_mlp.sh
@@ -7,7 +7,7 @@ sbatch <<'EOF'
 #SBATCH --ntasks=1                                                            # Number of tasks
 #SBATCH --gres=gpu:1                                                          # Request 1 GPU
 #SBATCH --time=24:00:00                                                       # Max wall time (HH:MM:SS)
-#SBATCH --mem=5G                                                              # Memory requirement
+#SBATCH --mem=10G                                                              # Memory requirement
 #SBATCH --partition=gpu_h100,gpu_a100_il,gpu_h100_il                          # Specify the GPU partition gpu_mi300
 
 cd ..
