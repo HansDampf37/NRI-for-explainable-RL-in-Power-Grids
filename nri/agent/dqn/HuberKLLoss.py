Fix logging and weighting bug

nri/agent/dqn/HuberKLLoss.py
@@ -50,8 +50,8 @@ class HuberKLLoss(nn.Module):
         huber = F.smooth_l1_loss(predictions, target)
         kl = self.kl_divergence_to_prior(posterior_probs)
         # Combine loss terms
-        kl_weight = huber.detach().abs() * self.beta  # scale the weight with ppo magnitude to prevent overshadowing
-        loss = (self.alpha * huber + kl_weight * kl) / (self.alpha + kl_weight)
+        kl_weight = huber.detach().abs().item() * self.beta  # scale the weight with huber magnitude to prevent overshadowing
+        loss = (self.alpha * huber + kl_weight * kl) / (self.alpha + self.beta)
         return (loss, huber, kl) if with_huber_kl else loss
 
 

nri/agent/ppo/RAPPO.py
@@ -216,9 +216,9 @@ class RAPPO(PPO, RARL):
         self.logger.record("train/value_loss", np.mean(value_losses))
         self.logger.record("train/approx_kl", np.mean(approx_kl_divs))
         self.logger.record("train/clip_fraction", np.mean(clip_fractions))
-        self.logger.record("train/total_loss", total_loss)
-        self.logger.record("train/ppo_loss", ppo_loss)
-        self.logger.record("train/weighted_kl", kl_weight * kl_loss)
+        self.logger.record("train/total_loss", total_loss.item())
+        self.logger.record("train/ppo_loss", ppo_loss.item())
+        self.logger.record("train/weighted_kl", (kl_weight * kl_loss).item())
         self.logger.record("train/kl-div", np.mean(kl_divs))
         self.logger.record("train/explained_variance", explained_var)
         if hasattr(self.policy, "log_std"):
@@ -236,7 +236,7 @@ class RAPPO(PPO, RARL):
         if tb_formatter is not None:
             writer = tb_formatter.writer  # this is the SummaryWriter
             mean_posterior = np.mean(mean_posteriors, axis=0)  # mean over iterations -> [E, K]
-            writer.add_histogram("latent_edges/mean posterior", mean_posterior, global_step=self.num_timesteps)
+            writer.add_histogram("latent_edges/mean posterior", mean_posterior[:, :-1], global_step=self.num_timesteps)
             hist_image = visualize_posterior(mean_posterior, self.prior.detach().cpu().numpy())
             writer.add_figure("latent_edges/posterior_vs_prior", hist_image, global_step=self.num_timesteps)
             if self.plotting_args is not None: