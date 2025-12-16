3b3d72dceb41784855f612c3a0da92575d42022b
Adrian Degenkolb <adrian@degenkolb.de>
2025-12-16 22:28:16 +0100

    Add CPU job scripts for PPO MLP baseline and Optuna training

--- slurm_scripts/optuna_cpu.sh
+++ slurm_scripts/optuna_cpu.sh
@@ -6,17 +6,16 @@ cd ..
 sbatch << EOF
 #!/bin/bash
 
-#SBATCH --job-name=optuna                                               # Name of the job
+#SBATCH --job-name=optuna                                                     # Name of the job
 #SBATCH --output=data/experiments/${experiment_name}/out/optuna.%j.log        # Output file
 #SBATCH --error=data/experiments/${experiment_name}/out/optuna_error.%j.log         # Error file
 #SBATCH --ntasks=1                                                            # Number of tasks
-#SBATCH --gres=gpu:1                                                          # Request 1 GPU
 #SBATCH --time=8:00:00                                                       # Max wall time (HH:MM:SS)
 #SBATCH --mem=10G                                                             # Memory requirement
-#SBATCH --partition=gpu_h100,gpu_a100_il,gpu_h100_il                          # Specify the GPU partition gpu_mi300
+#SBATCH --partition=cpu_il,cpu                          # Specify the GPU partition gpu_mi300
 
 module load devel/miniforge
 conda activate RL
 
-python test_cuda.py && PYTHONPATH=$(pwd) python training_scripts/optuna_rappo.py experiment_name=${experiment_name} timeout="06:00:00"
+PYTHONPATH=$(pwd) python training_scripts/optuna_rappo.py experiment_name=${experiment_name} timeout="06:00:00"
 EOF
--- slurm_scripts/train_baseline_ppo_mlp_cpu.sh
+++ slurm_scripts/train_baseline_ppo_mlp_cpu.sh
@@ -8,13 +8,12 @@ sbatch << EOF
 #SBATCH --output=data/experiments/${experiment_name}/out/train_baseline_mlp.%j.log            # Output file
 #SBATCH --error=data/experiments/${experiment_name}/out/error_train_baseline_mlp.%j.log       # Error file
 #SBATCH --ntasks=1                                                            # Number of tasks
-#SBATCH --gres=gpu:1                                                          # Request 1 GPU
 #SBATCH --time=12:00:00                                                       # Max wall time (HH:MM:SS)
 #SBATCH --mem=10G                                                             # Memory requirement
-#SBATCH --partition=gpu_h100,gpu_a100_il,gpu_h100_il                          # Specify the GPU partition gpu_mi300
+#SBATCH --partition=cpu_il,cpu                                        # Specify the GPU partition gpu_mi300
 
 module load devel/miniforge
 conda activate RL
 
-python test_cuda.py && PYTHONPATH=$(pwd) python training_scripts/train_mlp_ppo_baseline.py
+PYTHONPATH=$(pwd) python training_scripts/train_mlp_ppo_baseline.py
 EOF
