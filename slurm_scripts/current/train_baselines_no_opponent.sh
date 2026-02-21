f653a30f2f0614a7a9c7edcd8100575f9be65cde
Adrian Degenkolb <adrian@degenkolb.de>
2026-02-21 11:07:33 +0100

    Add configuration files and training scripts for baseline experiments

---
+++ slurm_scripts/current/train_baselines_no_opponent.sh
@@ -0,0 +1,44 @@
+experiment_name=26_02_21_baselines_gnn_and_mlp_no_opponent
+export experiment_name
+cd ../../
+
+# Create output directories
+mkdir -p results/experiments/${experiment_name}/out
+
+for seed in 0 1 2 3 4; do
+sbatch << EOF
+#!/bin/bash
+
+#SBATCH --job-name=gnn_s${seed}                                           # Name of the job
+#SBATCH --output=results/experiments/${experiment_name}/out/gnn_baseline_s${seed}.%j.log            # Output file
+#SBATCH --error=results/experiments/${experiment_name}/out/error_gnn_baseline_s${seed}.%j.log       # Error file
+#SBATCH --ntasks=1                                                            # Number of tasks
+#SBATCH --cpus-per-task=111                                                    # Number of CPU cores per task
+#SBATCH --time=20:00:00                                                       # Max wall time (HH:MM:SS)
+#SBATCH --mem=200G                                                            # Memory requirement
+#SBATCH --partition=cpu,cpu_il                          # Specify the GPU partition gpu_mi300
+
+module load devel/miniforge
+conda activate L2RPN
+
+PYTHONPATH=$(pwd) python training_scripts/train_ppo.py -f configs/rappo_anneal.yaml -wd . -s ${seed} -j 0 --model-type GNN --experiment-name ${experiment_name}
+EOF
+
+sbatch << EOF
+#!/bin/bash
+
+#SBATCH --job-name=mlp_s${seed}                                           # Name of the job
+#SBATCH --output=results/experiments/${experiment_name}/out/mlp_baseline_s${seed}.%j.log            # Output file
+#SBATCH --error=results/experiments/${experiment_name}/out/error_mlp_baseline_s${seed}.%j.log       # Error file
+#SBATCH --ntasks=1                                                            # Number of tasks
+#SBATCH --cpus-per-task=111                                                    # Number of CPU cores per task
+#SBATCH --time=20:00:00                                                       # Max wall time (HH:MM:SS)
+#SBATCH --mem=200G                                                            # Memory requirement
+#SBATCH --partition=cpu,cpu_il                          # Specify the GPU partition gpu_mi300
+
+module load devel/miniforge
+conda activate L2RPN
+
+PYTHONPATH=$(pwd) python training_scripts/train_ppo.py -f configs/rappo_anneal.yaml -wd . -s ${seed} -j 0 --model-type MLP --experiment-name ${experiment_name}
+EOF
+done
